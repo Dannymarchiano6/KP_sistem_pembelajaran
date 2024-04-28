ec5b3267990bbc20c5e13a7dc3ab0ebb18c4f183
dannymarchiano6 <dannymarchiano6@gmail.com>
2024-04-28 19:37:58 +0700

    Fix Menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -168,19 +168,40 @@ class HomePage extends StatelessWidget {
                     mainAxisSpacing: 10,
                     crossAxisSpacing: 10,
                   ),
-                  itemBuilder: (context, index)(
+                  itemBuilder: (context, index) {
                     return InkWell(
                       onTap: () {},
                       child: Container(
-                        padding: 
-                        EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          color: Color(0xFFFSF3FF)
+                          color: Color(0xFFF5F3FF),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                "images/${imgList[index]}.png",
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "55 Videos",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
               ],
             ),
